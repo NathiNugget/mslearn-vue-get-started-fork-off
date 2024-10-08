72f179c8f2f3ef0787e13252bd627285a3514154
= <nathaniel.risum2@gmail.com>
2024-10-08 14:45:05 +0200

    Added some starting practice functions

---
+++ start/assets/script.js
@@ -0,0 +1,11 @@
+const App = Vue.createApp({
+    // methods and content go here
+    data() {
+        return {
+            firstName: 'Christopher',
+            lastName: 'Harrison',
+            spaceShip: 'assets/images/spaceship-5730066-1284x856.jpg'
+        };
+    }
+});
+App.mount('#app');
